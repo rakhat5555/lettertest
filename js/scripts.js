108f091155ba733fcc0720245e1078a6c3d3a8f5
Student <example@example.com>
2019-02-06 20:49:27 -0800

    add method return to upper case

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var nameInput = $("input#name").val();
-    var lastNameInput = $("input#lastName").val();
+    var nameInput1 = $("input#name").val();
+    var nameInput = nameInput1.toUpperCase();
+
+    var lastNameInput1 = $("input#lastName").val();
+    var lastNameInput = lastNameInput1.toUpperCase();
 
   $(".name").text(nameInput);
   $(".lastName").text(lastNameInput);
   $("#letter").show();
+
 
   });
 });
